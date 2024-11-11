2968fadc293f17db584092dbfa7444031040ece3
bwissal13 <baazizwissal13@gmail.com>
2024-11-11 15:56:46 +0100

    add exeption

--- src/main/java/com/ebankify/api/exception/transactions/InactiveAccountException.java
+++
@@ -1,7 +0,0 @@
-package com.ebankify.api.exception.transactions;
-
-public class InactiveAccountException extends TransactionException {
-    public InactiveAccountException(String message) {
-        super(message);
-    }
-}
