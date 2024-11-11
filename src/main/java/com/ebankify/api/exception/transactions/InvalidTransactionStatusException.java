7b2ad3a8c89ec4c0c6fd8c7879b235d1128263d6
bwissal13 <baazizwissal13@gmail.com>
2024-11-11 15:57:23 +0100

    add exeption

--- src/main/java/com/ebankify/api/exception/transactions/InvalidTransactionStatusException.java
+++
@@ -1,7 +0,0 @@
-package com.ebankify.api.exception.transactions;
-
-public class InvalidTransactionStatusException extends TransactionException {
-    public InvalidTransactionStatusException(String message) {
-        super(message);
-    }
-}
