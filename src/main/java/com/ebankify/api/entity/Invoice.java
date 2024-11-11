ad79d8f0e265dab45db2d2e84495602df20717f0
bwissal13 <baazizwissal13@gmail.com>
2024-11-11 15:57:34 +0100

    add Invoice class

--- src/main/java/com/ebankify/api/entity/Invoice.java
+++ src/main/java/com/ebankify/api/entity/Invoice.java
@@ -1,8 +1,11 @@
 package com.ebankify.api.entity;
 
-import com.ebankify.api.entity.enums.InvoiceStatus;
+import com.ebankify.api.enums.InvoiceStatus;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,12 +14,9 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @Entity
-@Table(name = "invoices", indexes = {
-        @Index(name = "idx_invoice_id", columnList = "id")
-})
+@Table(name = "invoices")
 public class Invoice {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
