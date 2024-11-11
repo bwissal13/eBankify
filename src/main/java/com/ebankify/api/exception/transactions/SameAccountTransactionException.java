c87047e27fe564a12bcfce56aa74b5d37cb69526
bwissal13 <baazizwissal13@gmail.com>
2024-11-11 15:59:29 +0100

    update exeption

--- src/main/java/com/ebankify/api/exception/transactions/SameAccountTransactionException.java
+++
@@ -1,7 +0,0 @@
-package com.ebankify.api.exception.transactions;
-
-public class SameAccountTransactionException extends TransactionException {
-    public SameAccountTransactionException(String message) {
-        super(message);
-    }
-}
