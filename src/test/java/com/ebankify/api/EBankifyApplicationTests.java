dd86809e86e8b3eda9d0fc5da243949da4061329
bwissal13 <baazizwissal13@gmail.com>
2024-12-19 09:41:52 +0100

    Test

--- src/test/java/com/ebankify/api/EBankifyApplicationTests.java
+++ src/test/java/com/ebankify/api/EBankifyApplicationTests.java
@@ -3,8 +3,10 @@ package com.ebankify.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EBankifyApplicationTests {
 
     @Test
